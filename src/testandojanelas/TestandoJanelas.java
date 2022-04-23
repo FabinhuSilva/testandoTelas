8f88bf28dbd63159faadf94778a38610a983099a
FabinhuSilva <fabiano.fesilva@gmail.com>
2022-04-23 12:32:13 -0300

    testando telas

---
+++ src/testandojanelas/TestandoJanelas.java
@@ -0,0 +1,16 @@
+
+package testandojanelas;
+
+/**
+ *
+ * @author Fabinhu
+ */
+public class TestandoJanelas {
+
+    public static void main(String[] args) {
+
+        System.out.println("Testando Telas");
+        // TODO code application logic here
+    }
+    
+}
